e7d029beaa6408830c334390a4e437675b00e32f
Tanishka <guptatanishka2008@gmail.com>
2024-08-31 13:03:16 +0530

    add context API files

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import { useContext} from "react"
+import UserContext from "../context/Usercontext"
+
+const Profile = () => {
+  const {user} = useContext(UserContext)
+ 
+  if(!user) return <div>Please Login</div>
+
+  return <div>welcome {user.username}</div>
+}
+
+export default Profile
